55922a26e62e9260aea0ccf8a3f96d003f6605e6
Yeerane <xintai34000@126.com>
2016-04-14 17:24:01 +0800

    LeetCode 012

---
+++ Solution012.java
@@ -0,0 +1,23 @@
+public class Solution012 {
+	public String intToRoman(int num) {
+		int[] numUnits = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+		String[] romanUnits = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+		String roman = "";
+		while(num > 0) {
+			int length = numUnits.length;
+			for(int i = length - 1; i >= 0; i --) {
+				if(num >= numUnits[i]) {
+					num -= numUnits[i];
+					roman += romanUnits[i];
+					break;
+				}
+			}
+		}
+		return roman;
+	}
+
+	public static void main(String[] args) {
+		Solution012 test = new Solution012();
+		System.out.println(test.intToRoman(20));
+	}
+}
