1c11aa107ef4ea0d1782c8ac69f1a9772fc98da8
Yeerane <xintai34000@126.com>
2016-04-20 23:57:51 +0800

    combinations

--- combinations.java
+++ combinations.java
@@ -10,18 +10,12 @@ class Solution077 {
 			result.add(list);
 			System.out.println(result);
 		} else {
-			// System.out.println("list size " + list.size() + " is less than " + k + ", go on");
-				// System.out.println("before for");
 			for(int i = curIndex; i <= n; i ++) {
-				// System.out.println("put in " + i);
 				if(n - curIndex + 1 < k - list.size()) {
 					continue;
 				}
 				list.add(i);
-				// System.out.println(list);
 				result = findCombination(result, list, n, k, i + 1);
-				// System.out.println(result);
-				// System.out.println(list);
 				list.remove(list.size() - 1);
 			}
 			
@@ -38,6 +32,5 @@ class Solution077 {
     public static void main(String[] args) {
     	Solution077 test = new Solution077();
     	test.combine(4,2);
-    	// System.out.println(test.combine(4,2));
     }
 }
